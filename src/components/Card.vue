531892ee0eba458a8e78809071bbb05f0d288d38
Danh Ngoc <nguyendanhngocndn@gmail.com>
2020-04-19 21:25:26 +0700

    Update Code

---
+++ src/components/Card.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="conatiner">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h3 class="panel-title">Trello Clone</h3>
+          </div>
+          <div class="panel-body">
+            <div class="col-md-4">
+              <h3>Todo</h3>
+              <ul v-if="todos && todos.length" class="list-group">
+                <li v-for="todo in todos" :key="todo.id" class="list-group-item itemtodo">
+                  {{todo.title}}
+                  <a
+                    v-on:click="deleteTitle($event,todo.id)"
+                    class="btn btn-danger buttondelete"
+                    href
+                  >Delete</a>
+                </li>
+              </ul>
+              <input type="text" class="form-control" v-model="titles" placeholder="Input Title" />
+              <button v-on:click="createTodo($event)" type="submit" class="btn btn-primary">Add</button>
+            </div>
+            <div class="col-md-4">
+              <h3>Doings</h3>
+              <ul v-if="doings && doings.length" class="list-group">
+                <li
+                  v-for="todo in doings"
+                  :key="todo.id"
+                  class="list-group-item itemtodo"
+                >{{todo.title}}</li>
+              </ul>
+              <input
+                type="text"
+                class="form-control"
+                v-model="titledoing"
+                placeholder="Input Title"
+              />
+              <button v-on:click="createTododoing($event)" type="submit" class="btn btn-primary">Add</button>
+            </div>
+            <div class="col-md-4">
+              <h3>Done</h3>
+              <ul v-if="dones && dones.length" class="list-group">
+                <li
+                  v-for="todo in dones"
+                  :key="todo.id"
+                  class="list-group-item itemtodo"
+                >{{todo.title}}</li>
+              </ul>
+              <input type="text" class="form-control" v-model="titledone" placeholder="Input Title" />
+              <button v-on:click="createTododone($event)" type="submit" class="btn btn-primary">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const baseURL = "http://localhost:8080/api";
+//const post = "http://localhost:8080/api/todo/insert"
+export default {
+  name: "card",
+  data() {
+    return {
+      todos: [],
+      doings: [],
+      dones: [],
+      titles: "",
+      titledoing: "",
+      titledone: ""
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`${baseURL}/toto/gettype?type=todo`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+      this.todos = res.data;
+      const resdata = await axios.get(`${baseURL}/toto/gettype?type=doing`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+      this.doings = resdata.data;
+      const resdone = await axios.get(`${baseURL}/toto/gettype?type=done`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+      this.dones = resdone.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  methods: {
+    async createTodo() {
+      try {
+        const res = await axios.post(
+          `${baseURL}/todo/insert`,
+          {
+            title: this.titles,
+            todo_type: "todo"
+          },
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*"
+            }
+          }
+        );
+        this.todos = [...this.todos, res.data];
+        this.titles = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async createTododoing() {
+      try {
+        const res = await axios.post(`${baseURL}/todo/insert`, {
+          title: this.titledoing,
+          todo_type: "doing"
+        });
+        this.doings = [...this.doings, res.data];
+        this.titledoing = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async createTododone() {
+      try {
+        const res = await axios.post(`${baseURL}/todo/insert`, {
+          title: this.titles,
+          todo_type: "done"
+        });
+        this.dones = [...this.dones, res.data];
+        this.titledone = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteTitle(e, id) {
+      e.preventDefault();
+      try {
+        return await axios.post(`${baseURL}/todo/delete/` + id, {
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.itemtodo {
+  text-align: left;
+}
+.buttondelete {
+  float: right;
+  overflow: hidden;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from "axios"
+axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
